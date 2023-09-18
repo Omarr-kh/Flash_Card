22375c2b6d063ba06ed045cce81f563ee7786592
Omar <omarkhaled1752003@yahoo.com>
2023-09-18 14:59:57 +0300

    comments

--- main.py
+++ main.py
@@ -7,21 +7,31 @@ BACKGROUND_COLOR = "#B1DDC6"
 card = {}
 
 # Words Data
-words_data = pd.read_csv("./data/french_words.csv")
-words_dict = words_data.to_dict(orient="records")
+try:
+    words_data =pd.read_csv("./data/yet_to_learn.csv")
+except FileNotFoundError:
+    words_data = pd.read_csv("./data/french_words.csv")
+finally:
+    words_dict = words_data.to_dict(orient="records")
 
-# Functions
+# Functions for Changing and flipping cards
 
 
 def change_card():
-    global card
-    card = random.choice(words_dict)
+    global card, timer_id
+    window.after_cancel(timer_id)
+
+    try:
+        card = random.choice(words_dict)
+    except IndexError:
+        print("There are no words!")
+        exit()
     french_word = card["French"]
 
     canvas.itemconfig(word_text, text=french_word, fill="black")
     canvas.itemconfig(lang_text, text="French", fill="black")
     canvas.itemconfig(background, image=front_photo)
-    window.after(2500, flip)
+    timer_id = window.after(2500, flip)
 
 
 def flip():
@@ -31,9 +41,19 @@ def flip():
     canvas.itemconfig(background, image=back_photo)
 
 
-# Window
+def known_word():
+    words_dict.remove(card)
+
+    yet_to_learn = pd.DataFrame(words_dict)
+    yet_to_learn.to_csv("./data/yet_to_learn.csv", index=False)
+
+    change_card()
+
+
+# Window Setup
 window = tk.Tk()
 window.config(background=BACKGROUND_COLOR, padx=50, pady=50)
+timer_id = window.after(2500, flip)
 
 # Canvas
 canvas = tk.Canvas(width=800, height=526,
@@ -43,6 +63,7 @@ front_photo = tk.PhotoImage(file="./images/card_front.png")
 back_photo = tk.PhotoImage(file="./images/card_back.png")
 background = canvas.create_image(400, 263, image=front_photo)
 
+# Canvas texts
 lang_text = canvas.create_text(
     400, 150, text="", font=("Ariel", 30, "italic"))
 word_text = canvas.create_text(
@@ -53,7 +74,7 @@ canvas.grid(row=0, column=0, columnspan=2)
 # Buttons
 tick_photo = tk.PhotoImage(file="./images/right.png")
 tick_button = tk.Button(
-    image=tick_photo, highlightthickness=0, borderwidth=0, command=change_card)
+    image=tick_photo, highlightthickness=0, borderwidth=0, command=known_word)
 tick_button.grid(row=1, column=0)
 
 x_photo = tk.PhotoImage(file="./images/wrong.png")
